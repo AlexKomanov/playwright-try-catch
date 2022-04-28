913a50d474e6029232eff95bdeb4c23949ea43f0
Alex <akomanov@seatgeek.com>
2022-04-28 14:49:55 +0300

    first commit

---
+++ playwright.config.ts
@@ -0,0 +1,39 @@
+import type { PlaywrightTestConfig } from '@playwright/test';
+import { devices } from '@playwright/test';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config();
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+const config: PlaywrightTestConfig = {
+  testDir: './tests',
+  /* Maximum time one test can run for. */
+  timeout: 240000,
+  expect: {
+    /**
+     * Maximum time expect() should wait for the condition to be met.
+     * For example in `await expect(locator).toHaveText();`
+     */
+    timeout: 5000
+  },
+ 
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    browserName: 'chromium',
+    launchOptions: {
+      channel: 'chrome',
+      headless: false
+    }
+  },
+
+
+};
+
+export default config;
---
+++ tests/try-catch.spec.ts
@@ -0,0 +1,37 @@
+import { test, expect, Page } from '@playwright/test';
+
+
+
+test.describe.only('Try Catch Example', async () => {
+    test('example', async ({ page }) => {
+        await page.goto('https://www.saucedemo.com/');
+        await page.locator('#user-name').fill('standard_user');
+        await page.locator('#password').fill('secret_sauce');
+        await page.locator('#login-button').click();
+
+        try {
+            const item = page.locator('[class="inventory_item_name"]');
+            let itemsCounter = await item.count();
+            for (let index = 0; index < itemsCounter; index++) {
+                const itemName = await item.nth(index).innerText();
+                if(itemName.includes('bla bla bla')) {
+                    await item.nth(index).click();
+                    await page.waitForTimeout(5000);
+                }
+                
+            }
+            throw new Error(`Element not Found`);
+        }
+        catch (error) {
+            await test.step(`${error}`, async() => {
+                throw new Error(error.toString());
+            })
+           
+  
+        }
+
+
+    });
+
+});
+
